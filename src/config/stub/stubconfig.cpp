1c1c3ba6ff2cb48cf990fa4b11f111bcffda6c47
zaic <zaic101@gmail.com>
2013-01-05 03:55:03 +0700

    New Year Commit

---
+++ src/config/stub/stubconfig.cpp
@@ -0,0 +1,35 @@
+#include "stubconfig.h"
+
+StubConfig::StubConfig(int x, int y) : real_x(x), real_y(y) {
+	real_data = new char[x * y];
+}
+
+StubConfig::~StubConfig() {
+	delete[] real_data;
+}
+
+int StubConfig::getRealDimSize(int dim) const {
+	switch(dim) {
+	case 0: return real_x;
+	case 1: return real_y;
+	default: return 1;
+	}
+	return 100500;
+}
+
+bool StubConfig::setFrame(int frame) {
+	if(frame < 0) return false;
+	if(frame > getFramesCount()) return false;
+	current_frame_id = frame;
+	memset(real_data, 0, real_x * real_y);
+	switch(frame) {
+	case 0:
+		real_data[real_x + 5] = 6;
+		break;
+	default:
+		real_data[real_x + 5 + frame] = 2;
+		real_data[real_x * (frame + 1) + 5] = 4;
+	break;
+	}
+	return true;
+}
---
+++ src/config/stub/stubconfig.h
@@ -0,0 +1,25 @@
+#pragma once
+
+#include "../config.h"
+#include <cstring>
+
+class StubConfig : public Config {
+	int real_x, real_y;
+	char *real_data;
+
+protected:
+	StubConfig();
+
+public:
+	StubConfig(int x, int y);
+	virtual ~StubConfig();
+
+	virtual bool setFrame(int frame);
+	virtual int getFramesCount() { return real_y - 2; }
+
+	virtual const char* getRealData() const { return real_data; }
+	virtual int getRealDimSize(int dim) const;
+	virtual int getRealDimSizeX() const { return real_x; }
+	virtual int getRealDimSizeY() const { return real_y; }
+	virtual int getRealDimSizeZ() const { return 1; }
+};
