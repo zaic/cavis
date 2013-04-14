a7492f9f92d5d416a4dfd4c2bdc0c40aad13c03e
Vadim Zaytsev <zaic101@gmail.com>
2013-04-14 21:59:59 +0700

    Update DLL test library.

--- src/config/dll/dlltest.h
+++ src/config/dll/dlltest.h
@@ -3,7 +3,11 @@
 #include <stdlib.h>
 #include <string.h>
 
-void init();
-void calc();
-void* data();
-void quit();
+typedef void model_t;
+typedef unsigned char arr2_u8_t;
+
+model_t *create(const char *);
+void destroy(model_t *);
+void make_step(model_t *);
+void print_rules(model_t *);
+arr2_u8_t *average(model_t *);
